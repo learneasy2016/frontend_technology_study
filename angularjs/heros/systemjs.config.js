b06be6053218adb0312cf4f35dcb19d46c7ecaa2
learneasy2016 <learneasy2016@github.com>
2016-10-27 08:06:39 -0400

    disable inbrowser transpiler

--- angularjs/heros/systemjs.config.js
+++ angularjs/heros/systemjs.config.js
@@ -6,6 +6,7 @@
  */
 (function (global) {
   System.config({
+    /*
     // DEMO ONLY! REAL CODE SHOULD NOT TRANSPILE IN THE BROWSER
     transpiler: 'ts',
     typescriptOptions: {
@@ -15,11 +16,14 @@
       'typescript': {
         "exports": "ts"
       }
-    },
+    },*/
+
     paths: {
       // paths serve as alias
       'npm:': 'https://unpkg.com/'
     },
+    
+
     // map tells the System loader where to look for things
     map: {
       // our app is within the app folder
@@ -41,7 +45,6 @@
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
       'ts':                        'npm:plugin-typescript@4.0.10/lib/plugin.js',
       'typescript':                'npm:typescript@2.0.3/lib/typescript.js',
-
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
