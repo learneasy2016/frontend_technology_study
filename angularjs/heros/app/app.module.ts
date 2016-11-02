ff65bf64e439173468dfd68ae2b02c858dd60cca
learneasy2016 <learneasy2016@github.com>
2016-11-01 22:46:51 -0400

    With router

--- angularjs/heros/app/app.module.ts
+++ angularjs/heros/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
-    imports: [BrowserModule, FormsModule],
-    declarations: [AppComponent, HeroDetailComponent],
+    imports: [BrowserModule,
+        FormsModule,
+        AppRoutingModule],
+    declarations: [AppComponent, DashboardComponent, HeroDetailComponent, HeroesComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
